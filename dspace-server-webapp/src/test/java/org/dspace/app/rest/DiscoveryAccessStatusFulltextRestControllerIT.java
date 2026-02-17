71fe268e53a5ffccab7815d69c998afc8416eb73
Russ Poetker <rpoetke1@jh.edu>
2026-02-17 09:11:59 -0500

    Clean tests

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/DiscoveryAccessStatusFulltextRestControllerIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/DiscoveryAccessStatusFulltextRestControllerIT.java
@@ -122,82 +122,6 @@ public class DiscoveryAccessStatusFulltextRestControllerIT extends AbstractContr
     }
 
     @Test
-    public void discoverSearchObjectsTestWithContentInAPrivateBitstream() throws Exception {
-        //We turn off the authorization system in order to create the structure as defined below
-        context.turnOffAuthorisationSystem();
-
-        //** GIVEN **
-        //1. A community-collection structure with one parent community with sub-community and two collections.
-        parentCommunity = CommunityBuilder.createCommunity(context)
-            .withName("Parent Community")
-            .build();
-        Community child1 = CommunityBuilder.createSubCommunity(context, parentCommunity)
-            .withName("Sub Community")
-            .build();
-        Collection col1 = CollectionBuilder.createCollection(context, child1).withName("Collection 1").build();
-
-        //2. one public item that is readable by Anonymous
-        Item publicItem1 = ItemBuilder.createItem(context, col1)
-            .withTitle("Test")
-            .withIssueDate("2010-10-17")
-            .withAuthor("Smith, Donald")
-            .withSubject("ExtraEntry")
-            .build();
-
-        String bitstreamContent = "ThisIsSomeDummyText";
-
-        //Make the group that anon doesn't have access to
-        Group internalGroup = GroupBuilder.createGroup(context)
-            .withName("Internal Group")
-            .build();
-
-        //Add this bitstream with the internal group as the reader group
-        try (InputStream is = IOUtils.toInputStream(bitstreamContent, CharEncoding.UTF_8)) {
-            Bitstream bitstream = BitstreamBuilder.
-                createBitstream(context, publicItem1, is)
-                .withName("Bitstream")
-                .withDescription("Test Private Bitstream")
-                .withMimeType("text/plain")
-                .withReaderGroup(internalGroup)
-                .build();
-        }
-
-        //Run the filter media to be able to search on the text in the bitstream
-        runDSpaceScript("filter-media", "-f", "-i", publicItem1.getHandle());
-
-        //Turn on the authorization again to make sure that private/inaccessible items don't get show/used
-        context.restoreAuthSystemState();
-        context.setCurrentUser(null);
-        //** WHEN **
-        //An anonymous user browses this endpoint to find the objects in the system
-        //With a size 2
-        getClient().perform(get("/api/discover/search/objects")
-                .param("query", "ThisIsSomeDummyText"))
-
-            //** THEN **
-            //The status has to be 200 OK
-            .andExpect(status().isOk())
-            //The type has to be 'discover'
-            .andExpect(jsonPath("$.type", is("discover")))
-            //The page object needs to look like this
-            .andExpect(jsonPath("$._embedded.searchResult.page", is(
-                PageMatcher.pageEntry(0, 20)
-            )))
-            //Make sure that the item with the private bitstream doesn't show up
-            .andExpect(jsonPath("$._embedded.searchResult._embedded.objects", Matchers.not(
-                Matchers.contains(
-                    SearchResultMatcher.matchOnItemName("item", "items", "Test")
-            ))))
-            //These facets have to show up in the embedded.facets section as well with the given hasMore
-            // property because we don't exceed their default limit for a hasMore true (the default is 10)
-            .andExpect(jsonPath("$._embedded.facets", Matchers.containsInAnyOrder(defaultFacetMatchers)))
-            //There always needs to be a self link available
-            .andExpect(jsonPath("$._links.self.href", containsString("/api/discover/search/objects")))
-        ;
-
-    }
-
-    @Test
     public void discoverSearchObjectsTestWithContentInAPrivateBitstreamAnonymous() throws Exception {
         // ** GIVEN **
         setupItemWithPrivateBitstream();
@@ -467,7 +391,7 @@ public class DiscoveryAccessStatusFulltextRestControllerIT extends AbstractContr
     }
 
     @Test
-    public void discoverSearchObjectsTestWithContentInAnEmbargoedBitstreamAuthenticated() throws Exception {
+    public void discoverSearchObjectsTestWithContentInAnEmbargoedBitstreamAuthenticatedNoAccess() throws Exception {
         //** GIVEN **
         setupItemWithEmbargoedBitstream();
         String epersonToken = getAuthToken(eperson.getEmail(), password);
@@ -492,6 +416,40 @@ public class DiscoveryAccessStatusFulltextRestControllerIT extends AbstractContr
                 Matchers.contains(
                     SearchResultMatcher.matchOnItemName("item", "items", "TestEmbargoBitstreamItem")
             ))))
+            //These facets have to show up in the embedded.facets section as well with the given hasMore
+            // property because we don't exceed their default limit for a hasMore true (the default is 10)
+            .andExpect(jsonPath("$._embedded.facets", Matchers.containsInAnyOrder(defaultFacetMatchers)))
+            //There always needs to be a self link available
+            .andExpect(jsonPath("$._links.self.href", containsString("/api/discover/search/objects")))
+        ;
+    }
+
+    @Test
+    public void discoverSearchObjectsTestWithContentInAnEmbargoedBitstreamAuthenticatedAccess() throws Exception {
+        //** GIVEN **
+        setupItemWithEmbargoedBitstream(eperson);
+        String epersonToken = getAuthToken(eperson.getEmail(), password);
+
+        //** WHEN **
+        //An authenticated user browses this endpoint to find the objects in the system
+        //With a size 2
+        getClient(epersonToken).perform(get("/api/discover/search/objects")
+                .param("query", "ThisIsSomeDummyEmbargoedText"))
+
+            //** THEN **
+            //The status has to be 200 OK
+            .andExpect(status().isOk())
+            //The type has to be 'discover'
+            .andExpect(jsonPath("$.type", is("discover")))
+            //The page object needs to look like this
+            .andExpect(jsonPath("$._embedded.searchResult.page", is(
+                PageMatcher.pageEntry(0, 20)
+            )))
+            //Make sure that the item with the embargoed bitstream doesn't show up, search by authenticated user
+            .andExpect(jsonPath("$._embedded.searchResult._embedded.objects", Matchers.not(
+                Matchers.contains(
+                    SearchResultMatcher.matchOnItemName("item", "items", "TestEmbargoBitstreamItem")
+                ))))
             //These facets have to show up in the embedded.facets section as well with the given hasMore
             // property because we don't exceed their default limit for a hasMore true (the default is 10)
             .andExpect(jsonPath("$._embedded.facets", Matchers.containsInAnyOrder(defaultFacetMatchers)))
@@ -557,6 +515,10 @@ public class DiscoveryAccessStatusFulltextRestControllerIT extends AbstractContr
     }
 
     private void setupItemWithEmbargoedBitstream() throws Exception {
+        setupItemWithEmbargoedBitstream(null);
+    }
+
+    private void setupItemWithEmbargoedBitstream(EPerson groupMember) throws Exception {
         context.turnOffAuthorisationSystem();
 
         //1. A community-collection structure with one parent community with sub-community and two collections.
@@ -580,13 +542,22 @@ public class DiscoveryAccessStatusFulltextRestControllerIT extends AbstractContr
 
         //Add this bitstream with an embargo set for anonymous group
         try (InputStream is = IOUtils.toInputStream(bitstreamContent, CharEncoding.UTF_8)) {
-            BitstreamBuilder.
+            BitstreamBuilder bitstreamBuilder = BitstreamBuilder.
                 createBitstream(context, publicItem1, is)
                 .withName("Bitstream")
                 .withDescription("Test Private Bitstream")
                 .withMimeType("text/plain")
-                .withEmbargoPeriod(Period.ofMonths(12))
-                .build();
+                .withEmbargoPeriod(Period.ofMonths(12));
+
+            if (groupMember != null) {
+                Group internalGroup= GroupBuilder.createGroup(context)
+                    .withName("Internal Group")
+                    .addMember(groupMember)
+                    .build();
+                bitstreamBuilder.withReaderGroup(internalGroup);
+            }
+
+            bitstreamBuilder.build();
         }
 
         //Run the filter media to be able to search on the text in the bitstream
